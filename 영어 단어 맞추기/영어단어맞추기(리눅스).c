59b3eb30e43daa1a340208e1a5d006b66a85dac9
lee1201zxc <lee1201zxcv@naver.com>
2021-12-04 04:35:02 +0900

    Create 영어단어맞추기(리눅스).c

---
+++ 영어 단어 맞추기/영어단어맞추기(리눅스).c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<window.h>
+#include<string.h>
+#include<stdlib.h>
+void menu(void);
+void read(void);
+typedef struct list
+{
+	char e[16];
+	char k[63];
+}list;
+typedef struct line
+{
+	struct list *adrs;
+	struct line *next;
+}line;
+typedef struct line *link;
+void read(void)
+{
+	link az,head,juso;
+	FILE *f;
+	f=fopen("dic.txt","r");
+	int i=0;
+	while(1)
+	{
+		head=(line*)malloc(sizeof(line));
+		head->adrs=(list*)malloc(sizeof(list));
+		if(fscanf(f,"%s %s",head->adrs->e,head->adrs->k)==EOF)
+		{
+			break;
+		}
+		if(i==0)
+		{
+			juso=head;
+			i++;
+		}
+		else
+		{
+			az->next=head;
+		}
+		az=head;
+	}
+	link k=juso;
+	while(k->next!=NULL)
+	{
+		link j=k;
+		while(j->next!=NULL)
+		{
+			if(strcmp(k->adrs->e,j->adrs->e)>0)
+			{
+				list *kan=k->adrs;
+				k->adrs=j->adrs;
+				j->adrs=kan;
+			}
+			j=j->next;
+		}
+		k=k->next;
+	}
+	az=juso;
+	int correct=0,incorrect=0,m=0;
+	char answer[63];
+	do
+	{
+		printf("%s -> ",az->adrs->k);
+		scanf("%s",answer);
+		if(strcmp(answer,az->adrs->e)==0)
+		{
+			correct++;
+			printf("correct!\n");
+		}
+		else
+		{
+			printf("incorrect!\n");
+			incorrect++;
+		}
+		az=az->next;
+		m++;
+		if(strcmp(answer,".quit")==0)
+			incorrect--;
+	}while(strcmp(answer,".quit")!=0&&az->next!=NULL);
+	printf("당신의 점수는%.2f점입니다.",(double)100*(correct)/(correct+incorrect));
+	char r;
+	getchar();
+	r=getchar();
+	if(r=='\n')
+		menu();
+	fclose(f);
+}
+void  menu(void)
+{
+	system("clear");
+	link h;
+	int n;
+	printf(">> 영어 단어 맞추기 프로그램 <<\n");
+	printf("1.영어 단어 맞추기\t2. 프로그램 종료\n");
+	printf("\n번호를 선택하세요:");
+	scanf("%d",&n);
+	if(n==1)
+	{
+		read();
+	
+	}
+	else if(n==2)
+	{
+		system("clear");
+	}
+}
+int main(void)
+{
+	menu();
+	return 0;
+}
